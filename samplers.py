32ca98301955821301fc9e65f2a8e74e0636a5e6
Pam <pamhome21@gmail.com>
2024-07-14 02:28:23 +0500

    Add dpmpp_3m_sde CFG++

--- samplers.py
+++ samplers.py
@@ -4,7 +4,13 @@ from . import ppm_cfgpp_sampling
 
 INITIALIZED = False
 CFGPP_SAMPLER_NAMES_ORIGINAL = ["euler_cfg_pp", "euler_ancestral_cfg_pp"]
-CFGPP_SAMPLER_NAMES = CFGPP_SAMPLER_NAMES_ORIGINAL + ["dpmpp_2m_cfg_pp", "dpmpp_2m_sde_cfg_pp", "dpmpp_2m_sde_gpu_cfg_pp"]
+CFGPP_SAMPLER_NAMES = CFGPP_SAMPLER_NAMES_ORIGINAL + [
+    "dpmpp_2m_cfg_pp",
+    "dpmpp_2m_sde_cfg_pp",
+    "dpmpp_2m_sde_gpu_cfg_pp",
+    "dpmpp_3m_sde_cfg_pp",
+    "dpmpp_3m_sde_gpu_cfg_pp",
+]
 
 
 def inject_samplers():
@@ -20,6 +26,7 @@ class CFGPPSamplerSelect:
         return {
             "required": {
                 "sampler_name": (CFGPP_SAMPLER_NAMES,),
+                "eta": ("FLOAT", {"default": 1.0, "min": 0.0, "max": 100.0, "step": 0.01, "round": False}),
             }
         }
 
@@ -28,10 +35,11 @@ class CFGPPSamplerSelect:
 
     FUNCTION = "get_sampler"
 
-    def get_sampler(self, sampler_name):
+    def get_sampler(self, sampler_name, eta: float):
         if sampler_name in CFGPP_SAMPLER_NAMES_ORIGINAL:
             sampler_func = getattr(k_diffusion_sampling, "sample_{}".format(sampler_name))
         else:
             sampler_func = getattr(ppm_cfgpp_sampling, "sample_{}".format(sampler_name))
-        sampler = KSAMPLER(sampler_func)
+        extra_options = {} if sampler_name in {"euler_cfg_pp", "dpmpp_2m_cfg_pp"} else {"eta": eta}
+        sampler = KSAMPLER(sampler_func, extra_options=extra_options)
         return (sampler,)
