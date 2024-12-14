b556ab50d251a7419ace634bd5cd9cbf89bb38f8
Pam <pamhome21@gmail.com>
2024-12-14 15:14:38 +0500

    turn guidance limiter into post_cfg_function

--- nodes/guidance_limiter.py
+++ nodes/guidance_limiter.py
@@ -21,45 +21,26 @@ class GuidanceLimiter:
     CATEGORY = "model_patches/unet"
 
     def patch(self, model, sigma_start: float, sigma_end: float, cfg_rescale: float):
-        m = model.clone()
-
         def limited_cfg(args):
-            cond = args["cond"]
-            uncond = args["uncond"]
+            x_cfg = args["denoised"]
+            cond = args["cond_denoised"]
             sigma = args["sigma"]
-            cond_scale = args["cond_scale"]
 
-            if sigma_start >= 0 and sigma[0] > sigma_start:
-                cond_scale = 1
+            if (sigma_start >= 0 and sigma[0] > sigma_start) or (sigma_end >= 0 and sigma[0] <= sigma_end):
+                return cond
 
-            if sigma_end >= 0 and sigma[0] <= sigma_end:
-                cond_scale = 1
+            if cfg_rescale == 0.0:
+                return x_cfg
 
-            if cfg_rescale > 0:
-                x_orig = args["input"]
-                sigma = sigma.view(sigma.shape[:1] + (1,) * (cond.ndim - 1))
+            ro_pos = torch.std(cond, dim=(1, 2, 3), keepdim=True)
+            ro_cfg = torch.std(x_cfg, dim=(1, 2, 3), keepdim=True)
 
-                # rescale cfg has to be done on v-pred model output
-                x = x_orig / (sigma * sigma + 1.0)
-                cond = ((x - (x_orig - cond)) * (sigma**2 + 1.0) ** 0.5) / (sigma)
-                uncond = ((x - (x_orig - uncond)) * (sigma**2 + 1.0) ** 0.5) / (sigma)
+            x_rescaled = x_cfg * (ro_pos / ro_cfg)
+            x_final = cfg_rescale * x_rescaled + (1.0 - cfg_rescale) * x_cfg
+            return x_final
 
-                # rescalecfg
-                x_cfg = uncond + cond_scale * (cond - uncond)
-                ro_pos = torch.std(cond, dim=(1, 2, 3), keepdim=True)
-                ro_cfg = torch.std(x_cfg, dim=(1, 2, 3), keepdim=True)
-
-                x_rescaled = x_cfg * (ro_pos / ro_cfg)
-                x_final = cfg_rescale * x_rescaled + (1.0 - cfg_rescale) * x_cfg
-
-                return x_orig - (x - x_final * sigma / (sigma * sigma + 1.0) ** 0.5)
-
-            cfg_result = uncond + (cond - uncond) * cond_scale
-
-            return cfg_result
-
-        m.set_model_sampler_cfg_function(limited_cfg)
-
+        m = model.clone()
+        m.set_model_sampler_post_cfg_function(limited_cfg)
         return (m,)
 
 
